8d50cb49e25f596bd7adb726cee12cb86a2893f6
Evandro <evvolpe@gmail.com>
2022-05-10 19:44:01 -0300

    Correção exemplo-v1

--- proj/bundle/bundle.sh
+++ proj/bundle/bundle.sh
@@ -9,4 +9,4 @@ name=$2
 # Nome da pipeline
 template=$3
 
-tkn bundle push index.docker.io/clodonil/$template-$name:v1 -f $file
+tkn bundle push index.docker.io/winterfoxoutlook/$template-$name:v1 -f $file
